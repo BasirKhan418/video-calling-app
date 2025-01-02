8be62a1a6ad4c72fd5a28b9d4d2436691785c8db
Basir Khan <khanbasir5555@gmail.com>
2025-01-02 22:46:21 +0530

    sender ,reciever frontend added

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,29 +1,34 @@
-import React, { useEffect, useMemo } from 'react'
-import { io } from 'socket.io-client'
+import React, { useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
-    
-    const socket = useMemo(() => io('http://localhost:3000'), [])
+    const router = useNavigate()
+    const [streamCode, setStreamCode] = useState('')
 
-    useEffect(()=>{
-  socket.on('connect',()=>{
-console.log('Connected to server');
-  })
-
-  //disconnect from the server when the user leaves the page
-  const handleBeforeunload=()=>{
-    socket.disconnect();
-  }
-  window.addEventListener("beforeunload",handleBeforeunload);
-    return ()=>{
-      socket.disconnect();
-      window.addEventListener("beforeunload",handleBeforeunload);
+    const createStream=()=>{
+        const streamid = Math.floor(Math.random() * 1000000)+"DSWBKAS";
+        router(`/send?id=${streamid}`)
     }
-    },[])
+    const joinStream=()=>{
+        router(`/recieve?id=${streamCode}`)
+    }
+    
   return (
+    <>
     <div className='flex justify-center items-center flex-col'>
         <h1 className='text-red-600 text-4xl font-bold'>Home</h1>
-        <p className='text-blue-600 font-semibold mt-6'>Welcome to the home page</p>
+        <p className='text-blue-600 font-semibold mt-6'>Welcome to the basir's peer to peer video calling app.</p>
+        <p className='text-blue-400 font-light'>Implemente in pure webrtc(Web Real Time Communication).</p>
     </div>
+    <div className='flex justify-center items-center  mt-6 flex-col'>
+        <input type="text" className='border-2 border-green-600 p-2 rounded-lg lg:w-72' placeholder='Enter a Stream Code To Join' onChange={(e)=>setStreamCode(e.target.value)}/>
+        <div className='flex justify-center items-center flex-col md:flex-row lg:flex-row mt-4'>
+        <button className='bg-blue-600 text-white px-4 py-2 rounded-md mx-2 my-2' onClick={joinStream}>Join a Stream</button>
+        <button className='bg-red-600 text-white px-4 py-2 rounded-md mx-2 my-2' onClick={createStream}>Create a Stream</button>
+        </div>
+        
+    </div>
+    </>
+
   )
 }
 
--- frontend/src/components/Reciever.tsx
+++ frontend/src/components/Reciever.tsx
@@ -1,10 +1,52 @@
-import React from 'react'
 
+import React, { useEffect, useMemo } from 'react'
+import { io } from 'socket.io-client';
+import { FaRegCopy } from "react-icons/fa";
+import { useSearchParams } from 'react-router-dom';
 const Reciever = () => {
+    const [iscopy, setIscopy] = React.useState(false);
+    const socket = useMemo(() => io('http://localhost:3000'), [])
+    const [searchParams] = useSearchParams();
+    const id = searchParams.get('id');
+    useEffect(()=>{
+  socket.on('connect',()=>{
+console.log('Connected to server');
+  })
+
+  //disconnect from the server when the user leaves the page
+  const handleBeforeunload=()=>{
+    socket.disconnect();
+  }
+  window.addEventListener("beforeunload",handleBeforeunload);
+    return ()=>{
+      socket.disconnect();
+      window.addEventListener("beforeunload",handleBeforeunload);
+    }
+    },[])
   return (
-    <div>
-      hello  iam reciecer
-    </div>
+    <>
+      <div className='container bg-blue-400 p-4 flex justify-center items-center flex-col relative'>
+        <h1 className='text-xl font-extrabold '>Reciever</h1>
+        {iscopy&&<p className='text-black bg-white p-2 rounded-xl absolute '>Copied..</p>}
+        <h3 className='font-semibold flex justify-center items-center'>Stream ID: <span className='font-bold text-gray-900'>{id}</span> <FaRegCopy className='mx-2 text-green-800'
+        onClick={()=>{
+            setIscopy(true);
+            setTimeout(()=>{
+                setIscopy(false);
+            },1000)
+            if (id) {
+                navigator.clipboard.writeText(id);
+            }
+        }}
+        /></h3>
+        <p className='text-gray-600'>Share this stream id with other person you want to join the stream.</p>
+      </div>
+      <div className='flex justify-center items-center p-4 bg-gray-100 rounded m-1'>
+      <h1 className='text-lg font-light r'>
+        Waiting for the other person to join the stream.
+      </h1>
+      </div>
+    </>
   )
 }
 
