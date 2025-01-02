9595c64a27df2c42bfe663183c3dc1d445e6b854
Basir Khan <khanbasir5555@gmail.com>
2025-01-02 21:41:58 +0530

    basic github commit done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import React from 'react';
+import Home from './components/Home';
+import Reciever from './components/Reciever';
+import Sender from './components/Sender';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/send' element={<Sender/>}/>
+      <Route path='/recieve' element={<Reciever/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
---
+++ frontend/src/components/Sender.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sender = () => {
+  return (
+    <div>
+      hello iam sender
+    </div>
+  )
+}
+
+export default Sender
